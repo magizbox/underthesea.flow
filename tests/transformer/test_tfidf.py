8aceb20e2a5a10665e205e7b718a7ab185c75c6c
rain <brother.rain.1024@gmail.com>
2017-11-24 11:03:24 +0700

    add test

--- tests/transformer/test_tfidf.py
+++ tests/transformer/test_tfidf.py
@@ -5,10 +5,11 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 
 class TestTfidfVectorizer(TestCase):
     def test_tfidf(self):
-        text = ["My whole world changed from the moment I met you ",
-                "And it would never be the same",
-                "From the moment I heard your name "]
-        vectorizer = TfidfVectorizer(ngram_range=(1, 2))
+        text = ["tôi đi học",
+                "tôi ăn cơm",
+                "cơm rất ngon"]
+        vectorizer = TfidfVectorizer()
         text_tfidf = vectorizer.fit_transform(text)
         vocab = vectorizer.vocabulary_
-        pass
+        n = len(vocab)
+        self.assertGreater(n, 5)
