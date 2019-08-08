91bc7954bf0fc7afa6302195b187e2b7ad0a010a
Vu Anh <anhv.ict91@gmail.com>
2019-08-08 17:50:40 +0700

    GH-92: add dataset

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -1,8 +1,10 @@
 from unittest import TestCase, skip
 from languageflow.data_fetcher import DataFetcher, NLPData
 
+# @skip
+from languageflow.datasets import Dataset
 
-@skip
+
 class TestDataSets(TestCase):
 
     def test_uts2017_bank_sa(self):
@@ -20,3 +22,24 @@ class TestDataSets(TestCase):
     def test_vntc(self):
         corpus = DataFetcher.load_corpus(NLPData.VNTC)
         print(corpus)
+
+    def test_download(self):
+        # DataFetcher.download_data('VNESES')
+        name = 'UTS2017_BANK'
+        DataFetcher.download_data(name)
+        input('Please enter to continue: ')
+        DataFetcher.remove(name)
+
+    def test_download_2(self):
+        name = 'VLSP2018_SA'
+        url = 'https://www.dropbox.com/s/b9t2s23qfm66fq1/VLSP2018_SA.zip?dl=1'
+        DataFetcher.download_data(name, url)
+        input('Please enter to continue: ')
+        DataFetcher.remove(name)
+
+    def test_remove(self):
+        DataFetcher.remove('VNESES')
+
+    def test_get(self):
+        dataset = Dataset.get("VNESES")
+        print(0)
